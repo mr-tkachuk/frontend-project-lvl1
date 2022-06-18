ab128b6e9517ffeca26e88d7b585d4d98c9bba6b
Tkachuk Anton <mrtkachuk@inbox.ru>
2022-06-18 09:33:41 +0300

    complete step 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import {nameQuestion} from '../src/cli.js'
-
+import nameQuestion from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
-export function nameQuestion() {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+
+export default function nameQuestion() {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 }
