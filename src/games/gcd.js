2c0df876798323fe09c18058c675ba91a6ec2880
Tkachuk Anton <mrtkachuk@inbox.ru>
2022-06-24 23:00:13 +0300

    complete step 7

---
+++ src/games/gcd.js
@@ -0,0 +1,28 @@
+import playGame from '../index.js';
+import { getRandomNumber } from '../common-functions.js';
+
+const findGcd = (num1, num2) => {
+  if (num2 > num1) return findGcd(num2, num1);
+  if (!num2) return num1;
+  return findGcd(num2, num1 % num2);
+};
+
+const makePair = () => {
+  const min = 1;
+  const max = 100;
+  const firstNumber = getRandomNumber(min, max);
+  const secondNumber = getRandomNumber(min, max);
+  const question = `${firstNumber} ${secondNumber}`;
+  const correctAnswer = String(findGcd(firstNumber, secondNumber));
+  return [question, correctAnswer];
+};
+
+const brainEven = () => {
+  const testData = {
+    rule: 'Find the greatest common divisor of given numbers.',
+    makePair,
+  };
+  playGame(testData);
+};
+
+export default brainEven;
