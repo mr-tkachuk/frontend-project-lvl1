b766c9450498c161c2bcb25b8e4f6dde438228b4
Tkachuk Anton <mrtkachuk@inbox.ru>
2022-07-06 08:26:32 +0300

    fix isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,6 +2,7 @@ import playGame from '../index.js';
 import getRandomNumber from '../common-functions.js';
 
 const isPrime = (number) => {
+  if (number < 2) return false;
   for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) return false;
   }
